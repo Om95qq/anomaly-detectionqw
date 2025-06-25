e8c0910268b9e5c70fd2d68b787ea5e2fcafce6d
Om95qq <omkocharekar481@gmail.com>
2025-06-25 15:41:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,3 +103,9 @@ def download_file():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8501))  # default for Streamlit is 8501
+    os.system(f"streamlit run app.py --server.port {port} --server.address 0.0.0.0")
+
